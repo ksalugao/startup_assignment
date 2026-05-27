188184278cd1e2e9d029be950e999214d19856ab
ksalugao <kaisalugao@gmail.com>
2026-05-27 18:54:43 -0400

    Changed status

--- Product.js
+++ Product.js
@@ -1,36 +1,32 @@
 export default class Product {
-    constructor(name, quantity, lead_time, average_sales) {
-        this.name = name; 
-        this.quantity = quantity;
-        this.average_sales = average_sales;
-        this.lead_time = lead_time;
-    }
+  constructor(name, quantity, lead_time, average_sales) {
+    this.name = name;
+    this.quantity = quantity;
+    this.lead_time = lead_time;
+    this.average_sales = average_sales;
+    this.status = "HEALTHY";
+  }
 
-    getName() {
-        return this.name;
-    }
+  getQuantity() {
+    return this.quantity;
+  }
+  addQuantity(quantity) {
+    this.quantity += quantity;
+    this.calculateReorder();
+  }
+  calculateDaysRemaining() {
+    return Math.floor(this.quantity / this.average_sales);
+  }
+  calculateStatus() {
+    const diff_days = this.calculateDaysRemaining() - this.lead_time;
 
-    getQuantity() {
-        return this.quantity; 
+    // if > lead_time, "HEALTHY"; if < lead_time && > (20% lead_time); if < (20% lead_time)
+    if (diff_days > this.lead_time) {
+      this.status = "HEALTHY";
+    } else if (diff_days < this.lead_time) {
+      this.status = "LOW";
+    } else {
+      this.status = "CRITICAL";
     }
-
-    addQuantity(quantity) {
-        this.quantity += quantity;
-    }
-
-    getAverageSales() {
-        return this.average_sales; 
-    }
-
-    calculateDaysRemaining() {
-        return this.quantity / this.average_sales; 
-    }
-
-    needsReorder() {
-        return this.calculateDaysRemaining() <= this.lead_time; 
-    }
-
-    calculateStatus() {
-        this.reorder = this.calculateDaysRemaining() <= this.lead_time; 
-    }
+  }
 }
