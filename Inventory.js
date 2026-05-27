77ae9c44a2a2f42c552bcb1032666883d3a88032
Yat Wang <146673670+yatwang7@users.noreply.github.com>
2026-05-27 18:11:04 -0400

    table display

--- Inventory.js
+++ Inventory.js
@@ -1,21 +1,21 @@
 class Inventory {
     constructor(products = []) {
-      this.products = products; // Array of Product objects
+        this.products = products; // Array of Product objects
     }
 
     addProduct(product) {
-      this.products.push(product);
+        this.products.push(product);
     }
 
     removeProduct(product) {
-      this.products = this.products.filter(p => p !== product);
+        this.products = this.products.filter(p => p !== product);
     }
 
     getProduct(product) {
-      return this.products.find(p => p === product);
+        return this.products.find(p => p === product);
     }
 
     getProducts() {
-      return this.products;
+        return this.products;
     }
 }
