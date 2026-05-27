ef7803d162847fada4e3991576b854ac96b069fd
ksalugao <kaisalugao@gmail.com>
2026-05-27 18:05:24 -0400

    Added Product class

---
+++ script.js
@@ -0,0 +1,8 @@
+const product_name = document.getElementById("name");
+const inventory = document.getElementById("quantity");
+const sales = document.getElementById("sales");
+const days = document.getElementById("days");
+
+
+
+document.getElementById("submit").
